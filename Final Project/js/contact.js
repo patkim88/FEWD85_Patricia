2bd57cc7942cc23f7c883f59881c31d77781b2e2
patkim88 <kimpatricia@gmail.com>
2016-06-09 22:38:09 -0400

    menu left

---
+++ Final Project/js/contact.js
@@ -0,0 +1,31 @@
+$(function(){
+    $('nav a').mouseenter(function(){
+        $(this).css('color', '#709176');  
+    $(this).mouseout(function(){
+        $(this).css('color', '#cde77f');    
+        });
+    });
+});
+
+var myCenter=new google.maps.LatLng(40.760575,-73.767127);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:18,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomL
+
+google.maps.event.addDomListener(window, 'load', initialize);
